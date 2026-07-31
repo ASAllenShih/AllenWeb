a35003e66147d41dec37c6579f1184b12858bd0c
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2026-08-01 02:49:54 +0800

    新增系統角色至角色列舉

--- basic/util/integration/ai/data/message/role.php
+++ basic/util/integration/ai/data/message/role.php
@@ -5,6 +5,7 @@ namespace Allen\Basic\Util\Integration\Ai\Data\Message;
 enum Role: string
 {
 	case Developer = 'developer';
+	case System = 'system';
 	case User = 'user';
 	case Assistant = 'assistant';
 }
